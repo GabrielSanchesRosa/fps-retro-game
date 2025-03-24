2d98a715c9ef9dc0249a70b183b500a05ff67de3
Gabriel Sanches <gabrielsanches131202@gmail.com>
2025-03-24 08:09:41 -0300

    feat: implementing ammo system

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour {
     [SerializeField] private Camera gameCam;
+    [SerializeField] private int maxAmmo;
+    [SerializeField] private int ammo;
 
     void Start() {
         
@@ -13,13 +15,19 @@ public class PlayerAttack : MonoBehaviour {
 
     private void Shoot() {
         if (Input.GetButtonDown("Fire1")) {
-            Ray ray = gameCam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
-            RaycastHit  raycastHit;
+            if (ammo > 0) {
+                Ray ray = gameCam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+                RaycastHit  raycastHit;
 
-            if (Physics.Raycast(ray, out raycastHit)) {
-                Debug.Log("I'm lookin at: " + raycastHit.transform.name);
+                if (Physics.Raycast(ray, out raycastHit)) {
+                    Debug.Log("I'm lookin at: " + raycastHit.transform.name);
+                } else {
+                    Debug.Log("I'm lookin at nothing.");
+                }
+
+                ammo--;
             } else {
-                Debug.Log("I'm lookin at nothing.");
+                Debug.Log("No ammo.");
             }
         }
     }
