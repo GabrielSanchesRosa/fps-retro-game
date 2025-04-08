16c28cf74a242ccc8b331e850bd16162e48ae9b6
Gabriel Sanches <gabrielsanches131202@gmail.com>
2025-04-08 08:23:57 -0300

    feat: Implementing pause screen

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 public class GameManager : MonoBehaviour {
     public static GameManager instance;
 
-    [SerializeField] public bool playerAlive = true, haveSilverKey = false, haveGoldenKey = false;
+    [SerializeField] GameObject pausePanel;
+    [SerializeField] public bool playerAlive, haveSilverKey, haveGoldenKey;
 
     void Awake() {
         instance = this;
@@ -13,10 +14,30 @@ public class GameManager : MonoBehaviour {
         playerAlive   = true;
         haveSilverKey = false;
         haveGoldenKey = false;
+
+        Time.timeScale = 1f;
     }
 
     void Update() {
-        
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            PauseGame();
+        }
+    }
+
+    public void PauseGame() {
+        if (!pausePanel.activeSelf) {
+            Time.timeScale = 0f;
+            pausePanel.SetActive(true);
+
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible   = true;
+        } else {
+            Time.timeScale = 1f;
+            pausePanel.SetActive(false);
+
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible   = false;
+        }
     }
 
     public void GameOver() {
