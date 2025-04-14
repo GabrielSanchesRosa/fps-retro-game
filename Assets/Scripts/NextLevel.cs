dea1e6c7322c8c218711a33eb48014bcbd28d770
Gabriel Sanches <gabrielsanches131202@gmail.com>
2025-04-14 07:49:56 -0300

    feat: making the transition of levels

--- Assets/Scripts/NextLevel.cs
+++ Assets/Scripts/NextLevel.cs
@@ -1,12 +1,23 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class NextLevel : MonoBehaviour {
+    [SerializeField] private Animator oAnimatorTransition;
     [SerializeField] private string nextLevel;
+    [SerializeField] private float waitTime;
+
+    private IEnumerator LoadNextLevel() {
+        oAnimatorTransition.Play("ImageDarkening");
+        
+        yield return new WaitForSeconds(waitTime);
+
+        SceneManager.LoadScene(nextLevel);
+    }
 
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.CompareTag("Player")) {
-            SceneManager.LoadScene(nextLevel);
+            StartCoroutine(LoadNextLevel());
         }
     }
 }
